377b93d289c9d8787985fb6a73fb1c3dd087c16a
phatjk <48487157+phatjkk@users.noreply.github.com>
2021-09-15 15:01:06 +0700

    Update WiFi_Home_Automation_ESP8266.ino

--- WiFi_Home_Automation_ESP8266.ino
+++ WiFi_Home_Automation_ESP8266.ino
@@ -1,20 +1,4 @@
-/*************************************************************
-  By Shyam Ravi- theTechnowright
-  www.youtube.com/thetechnowright 
-  
-  Download latest Blynk library here:
-  https://github.com/blynkkk/blynk-library/releases/latest
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-  You’ll need:
-   - Blynk App (download from AppStore or Google Play)
-   - ESP8266 board
-   - Decide how to connect to Blynk
-     (USB, Ethernet, Wi-Fi, Bluetooth, ...
- *************************************************************/
-/* Comment this out to disable prints and save space */
+
 #define BLYNK_PRINT Serial
 
 
@@ -44,7 +28,4 @@ void setup()
 void loop()
 {
   Blynk.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
 }
